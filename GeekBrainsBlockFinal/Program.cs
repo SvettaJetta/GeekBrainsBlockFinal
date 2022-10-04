d564af5c21241c55f06201c8b8609d0d2c4b51b8
Andrey Kras <34101243+SvettaJetta@users.noreply.github.com>
2022-10-04 18:31:06 +0300

    add methods

--- GeekBrainsBlockFinal/Program.cs
+++ GeekBrainsBlockFinal/Program.cs
@@ -4,9 +4,54 @@ namespace GeekBrainsBlockFinal
 {
     class Program
     {
+        static string[] my_string_array_test = { "732", "hello", "-)", "test", "3", "www" };
+
+        static int flag = 3;
+
+        static int CheckArray(string[] array)
+        {
+            int n = 0;
+            foreach (string x in array)
+            {
+                if (x.Length <= flag)
+                {
+                    n++;
+                }
+            }
+
+            return n;
+        }
+        static string[] CreateNewStringArray(int n)
+        {
+            string[] str_array_with_three_chars_empty = new string[n];
+
+            return str_array_with_three_chars_empty;
+        }
+        static string[] FillEmptyArray(string[] array, string[] empty_array)
+        {
+            int i = 0;
+            foreach (string x in array)
+            {
+                if (x.Length <= flag)
+                {
+                    empty_array.SetValue(x, i);
+                    i++;
+                }
+            }
+            string[] str_array_with_three_chars = empty_array;
+            return str_array_with_three_chars;
+        }
+
+        static void PrintArray(string[] array)
+        {
+            foreach (string x in array)
+            {
+                Console.Write($"{ x } \t");
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
